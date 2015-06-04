7c843a0c56f435455814cc613d198b0cdd9927be
p1tr0 <pertras.butkus@outlook.com>
2015-06-04 18:05:58 +0100

    before sql

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -8,10 +8,31 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: HelloWorld
+        // GET: /HelloWorld/Index/
         public ActionResult Index()
         {
             return View();
         }
+
+        // GET: /HelloWorld/ 
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action...";
+        //} 
+
+        // GET: /HelloWorld/Welcome/
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    
+        // GET: /HelloWorld/Welcome/ 
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        //}
     }
 }
